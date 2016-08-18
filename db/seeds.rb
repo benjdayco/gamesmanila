9da0623103f920768550de42238e9e4d32f4ddc8
Benjie Dayco <benjie.dayco@gmail.com>
2016-08-18 15:22:32 +0800

    4.2 Database Migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(:name => "console-generated product", :description => "I created this product without using the HTML form!")
