6a7366dcdc91f872555f644304e6e3a8d9f4fd5c
Benjie Dayco <benjie.dayco@gmail.com>
2016-08-31 16:13:17 +0800

    5.4 Authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -76,4 +76,6 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :image_url, :color, :price)
     end
+
+    
 end
