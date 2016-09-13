e7e797ba2fdd95683e1ee4ff6b2a7ae3b64cfe7d
Benjie Dayco <benjie.dayco@gmail.com>
2016-09-13 15:23:34 +0800

    5.9.2 Validations and PAginations - added an alert for failed reviews

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
       else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.html { redirect_to @product, alert: 'Review was not saved successfully. Please leave a comment and a rating.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
