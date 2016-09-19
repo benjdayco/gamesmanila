00904e98fc21deee0861e4fb5ac2936e028623c5
Benjie Dayco <benjie.dayco@gmail.com>
2016-09-19 12:49:54 +0800

    5.10 Tweaks to user registration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
         :to => 'benjie.dayco@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def new_registration(user)
+  	@user = user
+  	message = "Welcome to Games Manila."
+  	mail(:to => @user.email, :subject => "Welcome to Games Manila!!!")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   end
   resources :users
 
+  devise_for :users, :controllers => { :registrations => "my_registrations" }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
