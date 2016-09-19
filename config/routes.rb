d6b8a57910226c722b835ce48bffdafa22af331d
Benjie Dayco <benjie.dayco@gmail.com>
2016-09-19 12:57:08 +0800

    5.11 Consumer Web App

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "my_registrations" }
   resources :users
   resources :products
   get 'static_pages/about'
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   resources :users
 
-  devise_for :users, :controllers => { :registrations => "my_registrations" }
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
